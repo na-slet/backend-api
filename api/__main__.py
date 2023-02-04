d951f85661f9647935129ab1d85d41b906c56b18
huscker <egostalker03@mail.ru>
2023-02-04 15:36:40 +0300

    Updated db source

--- api/__main__.py
+++ api/__main__.py
@@ -60,13 +60,7 @@ async def log_requst(request: Request, call_next):
     return response
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(union_router)
